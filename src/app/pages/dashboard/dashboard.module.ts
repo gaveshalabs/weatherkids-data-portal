024d124e20d492548ddc0047421d70ede11794a6
Lihini Senanayake <lihini.sena@gmail.com>
2023-09-17 23:50:16 +0530

    added ngx-admin template base

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbListModule,
+  NbIconModule,
+} from '@nebular/theme';
+// import { NgxEchartsModule } from 'ngx-echarts';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { DashboardComponent } from './dashboard.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
+import { WeatherComponent } from './weather/weather.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbTabsetModule,
+    NbActionsModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbListModule,
+    NbIconModule,
+    NbButtonModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    TemperatureDraggerComponent,
+    TemperatureComponent,
+    WeatherComponent,
+  ],
+})
+export class DashboardModule { }
