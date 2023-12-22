698e722119565e78a8c5528806a4042a7325d877
Lihini Senanayake <lihini.sena@gmail.com>
2023-12-22 21:59:43 +0530

    fix: app theme

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -1,28 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbSecurityModule } from '@nebular/security';
 import {
     NbActionsModule,
-    NbButtonModule,
     NbContextMenuModule,
-    NbIconModule,
     NbLayoutModule,
     NbMenuModule,
-    NbSearchModule,
-    NbSelectModule,
     NbSidebarModule,
     NbThemeModule,
     NbUserModule,
 } from '@nebular/theme';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {
-    FooterComponent,
-    HeaderComponent,
-    SearchInputComponent,
-    TinyMCEComponent,
-} from './components';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {
     OneColumnLayoutComponent,
     ThreeColumnsLayoutComponent,
@@ -36,31 +29,73 @@ import {
     RoundPipe,
     TimingPipe,
 } from './pipes';
-import { CORPORATE_THEME } from './styles/theme.corporate';
-import { COSMIC_THEME } from './styles/theme.cosmic';
-import { DARK_THEME } from './styles/theme.dark';
-import { DEFAULT_THEME } from './styles/theme.default';
 import { LoaderComponent } from './components/loader/loader.component';
+import { RouterModule } from '@angular/router';
+import { NbJSThemeOptions } from '@nebular/theme';
+
+const palette = {
+    primary: '#1976d2',
+    secondary: '#d27619',
+    success: '#89BF0B',
+    info: '#00D9ED',
+    warning: '#F2A100',
+    danger: '#FF381E',
+};
+const APP_THEME: NbJSThemeOptions = {
+    name: 'clownfish',  // dark blue and dark orange
+    base: 'default',
+    variables: {
+        fontMain: 'Outfit, Noto Sans Sinhala, sans-serif',
+        fontSecondary: 'Outfit, Noto Sans Sinhala, sans-serif',
+
+        bg: '#ffffff',
+        bg2: '#f5f5f5',
+        bg3: '#ebebeb',
+        bg4: '#e0e0e0',
+
+        border: '#ffffff',
+        border2: '#f5f5f5',
+        border3: '#ebebeb',
+        border4: '#e0e0e0',
+        border5: '#b3b3b3',
+
+        fg: '#838383',
+        fgHeading: '#1a2138',
+        fgText: '#1a2138',
+        fgHighlight: palette.primary,
+        layoutBg: '#ebebeb',
+        separator: '#ebebeb',
+
+        primary: palette.primary,
+        secondary: palette.secondary,
+        success: palette.success,
+        info: palette.info,
+        warning: palette.warning,
+        danger: palette.danger,
+
+        primaryLight: '#63a4ff',
+        // successLight: '#9FDA66',
+        // infoLight: '#59C7DD',
+        // warningLight: '#EBBC5E',
+        // dangerLight: '#DF6B6A',
+
+        primaryDark: '#004ba0',
+    },
+};
 
 const NB_MODULES = [
     NbLayoutModule,
     NbMenuModule,
     NbUserModule,
     NbActionsModule,
-    NbSearchModule,
     NbSidebarModule,
     NbContextMenuModule,
     NbSecurityModule,
-    NbButtonModule,
-    NbSelectModule,
-    NbIconModule,
     NbEvaIconsModule,
 ];
 const COMPONENTS = [
     HeaderComponent,
     FooterComponent,
-    SearchInputComponent,
-    TinyMCEComponent,
     OneColumnLayoutComponent,
     ThreeColumnsLayoutComponent,
     TwoColumnsLayoutComponent,
@@ -76,22 +111,22 @@ const PIPES = [
 ];
 
 @NgModule({
-    imports: [CommonModule, ...NB_MODULES, MatIconModule],
-    exports: [CommonModule, ...PIPES, ...COMPONENTS],
     declarations: [...COMPONENTS, ...PIPES],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        RouterModule,
+        ...NB_MODULES,
+    ],
+    exports: [CommonModule, ...COMPONENTS, ...PIPES],
 })
 export class ThemeModule {
     static forRoot(): ModuleWithProviders<ThemeModule> {
         return {
             ngModule: ThemeModule,
-            providers: [
-                ...NbThemeModule.forRoot(
-                    {
-                        name: 'default',
-                    },
-                    [DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME, DARK_THEME]
-                ).providers,
-            ],
+            providers: NbThemeModule.forRoot({ name: 'clownfish' }, [APP_THEME]).providers,
         };
     }
 }
