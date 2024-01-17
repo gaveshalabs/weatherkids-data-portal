f7a44c2afa7c4ccd853a678a839c2f144a090f76
Lihini Senanayake <lihini.sena@gmail.com>
2024-01-17 17:37:36 +0530

    fix: scroll bar in large screens

--- src/app/@theme/layouts/weather-portal/weather-portal.layout.ts
+++ src/app/@theme/layouts/weather-portal/weather-portal.layout.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'ngx-weather-portal-layout',
     styleUrls: ['./weather-portal.layout.scss'],
     template: `
-    <nb-layout>
+    <nb-layout windowMode>
       <nb-layout-column>
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
