757839a5820f053acd25f648f428c113e505a476
chethana-dissanayaka <cchethana173@gmail.com>
2024-07-30 16:22:29 +0530

    fix: fix prod environment and Update dashboard cards

--- src/app/pages/kite-competition/togal-card/toggle-card.component.ts
+++ src/app/pages/kite-competition/togal-card/toggle-card.component.ts
@@ -1,180 +1,9 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-// import { OAuth2Service } from '../../../modules/oauth2/oauth2.service';
-// import { UserProfile } from '../../../common/interfaces/user.interface';
-// import { filter, switchMap } from 'rxjs/operators';
-// import { Observable, of } from 'rxjs';
-// import { Player } from '../../../@components/leaderboard/leaderboard.interface';
-// import { environment } from '../../../../environments/environment';
-// import { HttpClient } from '@angular/common/http';
-
-// enum CurrentViewType {
-//     MyData,
-//     AllData,
-//     PlayerData
-// }
-
-// @Component({
-//     selector: 'ngx-toggle-card',
-//     templateUrl: './toggle-card.component.html',
-//     styleUrls: ['./toggle-card.component.scss'],
-// })
-// export class ToggleCardComponent implements OnInit {
-
-//     user: UserProfile;
-//     footerContent: string = 'You are viewing all players view';
-//     currentView: CurrentViewType = CurrentViewType.AllData;
-//     allViewTypes = CurrentViewType;
-//     players: Player[] = [];
-//     returnUrl: string;
-
-//     constructor(
-//         private router: Router,
-//         private route: ActivatedRoute,
-//         private oAuthService: OAuth2Service,
-//         private http: HttpClient,
-//     ) {}
-
-//     ngOnInit() {
-//         this.router.events
-//             .pipe(filter(event => event instanceof NavigationEnd))
-//             .subscribe(() => {
-//                 this.updateFooterContent();
-//             });
-
-//         this.oAuthService.getUser().subscribe(user => {
-//             if (user) {
-//                 localStorage.setItem('user_id', user._id); // Store user ID in local storage
-//                 this.user = user;
-//             } else {
-//                 localStorage.removeItem('user_id'); // Clear user ID from local storage
-//                 this.user = null;
-//             }
-//             this.updateFooterContent();
-//         });
-
-//         this.loadPlayers();
-//     }
-
-//     async onSignIn() {
-//         this.returnUrl = this.router.url; // Capture the current URL
-//         await this.oAuthService.login(this.returnUrl);
-//     }
-
-//     onViewMyDetails() {
-//         const userId = this.getUserIdFromLocalStorage();
-//         // console.log('User ID:', userId);
-
-//         this.currentView = CurrentViewType.MyData; // Set view to MyData
-
-//         if (userId) {
-//             this.footerContent = 'You are signed in, but you have not recorded any data yet.';
-//         } else {
-//             this.footerContent = 'You are not signed in. Please sign in.';
-//             this.onSignIn();
-//         }
-//     }
-
-//     onViewAllDetails() {
-//         this.currentView = CurrentViewType.AllData; // Set view to AllData
-//         this.router.navigate(['/kite/player/all']);
-//     }
-
-//     goToPlayerIntroduction(player: Player) {
-//         this.router.navigate(['/kite/player', player.id], {
-//             state: { player },
-//         });
-//         this.currentView = CurrentViewType.PlayerData; // Set view to PlayerData
-//         this.footerContent = 'You are viewing your details'; // Update footer content
-//     }
-
-//     private updateFooterContent() {
-//         const currentUrl = this.router.url;
-//         const userId = this.getUserIdFromLocalStorage();
-
-//         if (currentUrl.includes('/kite/player/')) {
-//             const playerId = this.route.snapshot.paramMap.get('id');
-//             if (playerId) {
-//                 this.currentView = CurrentViewType.PlayerData;
-//                 if (this.players.length) {
-//                     const player = this.players.find(p => p.id === playerId);
-//                     if (player) {
-//                         this.footerContent = `You are viewing ${player.name}'s details @${player.city}`;
-//                     } else {
-//                         this.footerContent = 'Player data not found';
-//                     }
-//                 } else {
-//                     this.footerContent = 'Loading player data...';
-//                 }
-//             }
-//         } else if (currentUrl === '/kite/player/all') {
-//             this.currentView = CurrentViewType.AllData;
-//             this.footerContent = 'You are viewing all players view';
-//         } else {
-//             this.currentView = CurrentViewType.AllData;
-//             this.footerContent = 'You are viewing all players view';
-//         }
-//     }
-
-//     fetchPlayerData(playerId: string): Observable<Player> {
-//         return new Observable<Player>(observer => {
-//             if (this.players.length) {
-//                 const player = this.players.find(p => p.id === playerId);
-//                 if (player) {
-//                     observer.next(player);
-//                     observer.complete();
-//                 } else {
-//                     observer.error('Player not found');
-//                 }
-//             } else {
-//                 observer.error('Players data not loaded');
-//             }
-//         }).pipe(
-//             switchMap(() => this.getPlayersLeaderboard().pipe(
-//                 switchMap(players => {
-//                     this.players = players;
-//                     const player = this.players.find(p => p.id === playerId);
-//                     if (player) {
-//                         return of(player);
-//                     } else {
-//                         throw new Error('Player not found');
-//                     }
-//                 })
-//             ))
-//         );
-//     }
-
-//     loadPlayers() {
-//         this.getPlayersLeaderboard().subscribe(players => {
-//             this.players = players;
-//             this.updateFooterContent(); // Ensure footer content is updated after players are loaded
-//         }, error => {
-//             console.error('Error fetching players leaderboard:', error);
-//         });
-//     }
-
-//     getPlayersLeaderboard(): Observable<Player[]> {
-//         return this.http.get<Player[]>(`${environment.apiBaseUrl}/kite-data/players-leaderboard`);
-//     }
-
-//     private getUserIdFromLocalStorage(): string | null {
-//         const user = localStorage.getItem('user');
-//         if (user) {
-//             const userProfile: UserProfile = JSON.parse(user);
-//             return userProfile._id;
-//         }
-//         return null;
-//     }
-// }
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { OAuth2Service } from '../../../modules/oauth2/oauth2.service';
 import { UserProfile } from '../../../common/interfaces/user.interface';
-import { filter, switchMap } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Player } from '../../../@components/leaderboard/leaderboard.interface';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
@@ -215,10 +44,10 @@ export class ToggleCardComponent implements OnInit {
 
         this.oAuthService.getUser().subscribe(user => {
             if (user) {
-                localStorage.setItem('user_id', user._id); // Store user ID in local storage
+                localStorage.setItem('user', JSON.stringify(user)); // Store user in local storage
                 this.user = user;
             } else {
-                localStorage.removeItem('user_id'); // Clear user ID from local storage
+                localStorage.removeItem('user'); // Clear user from local storage
                 this.user = null;
             }
             this.updateFooterContent();
@@ -234,12 +63,25 @@ export class ToggleCardComponent implements OnInit {
 
     onViewMyDetails() {
         const userId = this.getUserIdFromLocalStorage();
-        // console.log('User ID:', userId);
-
         this.currentView = CurrentViewType.MyData; // Set view to MyData
 
         if (userId) {
-            this.footerContent = 'You are signed in, but you have not recorded any data yet.';
+            this.getKitePlayerByUser(userId).subscribe(response => {
+                if (response && response._id) {
+                    this.router.navigate(['/kite/player', response._id]).then(() => {
+                        this.footerContent = 'You are viewing your player records';
+                    });
+                } else {
+                    this.footerContent = 'You are signed in, but you are not a player.';
+                }
+            }, error => {
+                if (error.status === 404) {
+                    this.footerContent = 'You are signed in, but you are not a player.';
+                } else {
+                    console.error('Error fetching player data:', error);
+                    this.footerContent = 'Error fetching player data.';
+                }
+            });
         } else {
             this.footerContent = 'You are not signed in. Please sign in.';
             this.onSignIn();
@@ -256,7 +98,7 @@ export class ToggleCardComponent implements OnInit {
             state: { player },
         });
         this.currentView = CurrentViewType.PlayerData; // Set view to PlayerData
-        this.footerContent = 'You are viewing your details'; // Update footer content
+        this.footerContent = `You are viewing ${player.name}'s details @${player.city}`; // Update footer content
     }
 
     private updateFooterContent() {
@@ -269,17 +111,22 @@ export class ToggleCardComponent implements OnInit {
         } else if (currentUrl.startsWith('/kite/player/') && currentUrl !== '/kite/player/all') {
             const playerId = this.route.snapshot.paramMap.get('id');
             if (playerId) {
-                this.currentView = CurrentViewType.PlayerData;
-                if (this.players.length) {
-                    const player = this.players.find(p => p.id === playerId);
-                    if (player) {
-                        this.footerContent = `You are viewing ${player.name}'s details @${player.city}`;
+                const player = this.players.find(p => p.id === playerId);
+                if (player) {
+                    if (userId && userId === playerId) {
+                        this.currentView = CurrentViewType.MyData;
+                        this.footerContent = 'You are viewing your player records';
                     } else {
-                        this.footerContent = 'Player data not found';
+                        this.currentView = CurrentViewType.PlayerData;
+                        this.footerContent = `You are viewing ${player.name}'s details @${player.city}`;
                     }
                 } else {
-                    this.footerContent = 'Loading player data...';
+                    this.currentView = CurrentViewType.PlayerData;
+                    this.footerContent = 'Player data not found';
                 }
+            } else {
+                this.currentView = CurrentViewType.PlayerData;
+                this.footerContent = 'You are viewing player records';
             }
         } else {
             this.currentView = CurrentViewType.AllData;
@@ -288,31 +135,7 @@ export class ToggleCardComponent implements OnInit {
     }
 
     fetchPlayerData(playerId: string): Observable<Player> {
-        return new Observable<Player>(observer => {
-            if (this.players.length) {
-                const player = this.players.find(p => p.id === playerId);
-                if (player) {
-                    observer.next(player);
-                    observer.complete();
-                } else {
-                    observer.error('Player not found');
-                }
-            } else {
-                observer.error('Players data not loaded');
-            }
-        }).pipe(
-            switchMap(() => this.getPlayersLeaderboard().pipe(
-                switchMap(players => {
-                    this.players = players;
-                    const player = this.players.find(p => p.id === playerId);
-                    if (player) {
-                        return of(player);
-                    } else {
-                        throw new Error('Player not found');
-                    }
-                })
-            ))
-        );
+        return this.http.get<Player>(`${environment.apiBaseUrl}/kite-data/player/${playerId}`);
     }
 
     loadPlayers() {
@@ -326,6 +149,10 @@ export class ToggleCardComponent implements OnInit {
 
     getPlayersLeaderboard(): Observable<Player[]> {
         return this.http.get<Player[]>(`${environment.apiBaseUrl}/kite-data/players-leaderboard`);
+    }
+
+    getKitePlayerByUser(userId: string): Observable<any> {
+        return this.http.get<any>(`${environment.apiBaseUrl}/kite-players/users/${userId}`);
     }
 
     private getUserIdFromLocalStorage(): string | null {
