57a399e94d9c92c59f3b39a947bba5cc93d27a82
chethana-dissanayaka <cchethana173@gmail.com>
2024-08-27 17:27:52 +0530

    chore: remove unwanted  dashboard cards

--- src/app/pages/kite-competition/want-to-know/want-to-know.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { TotalKiteData } from '../../../@components/leaderboard/leaderboard.interface';
-
-@Component({
-    selector: 'ngx-wanttoknow',
-    styleUrls: ['./want-to-know.component.scss'],
-    templateUrl: './want-to-know.component.html',
-})
-export class WantToKnowComponent  {}
